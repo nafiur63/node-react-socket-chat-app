5a75b3a7541408d85787bf9126b2feb488967417
nafiur63 <nafiur63@gmail.com>
2021-06-17 20:32:11 +0600

    files ready

---
+++ client/src/screens/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+const LoginPage = () => {
+    return (
+        <Container>
+            Hello Login
+        </Container>
+    )
+}
+
+export default LoginPage
